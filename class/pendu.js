9c12a587798c2d87057a42c6792472facc0bdafa
GROUSSON GAEL <g.grousson@wanadoo.fr>
2023-06-28 10:32:28 +0200

    update

--- class/pendu.js
+++ class/pendu.js
@@ -209,7 +209,7 @@ class Pendu{
           console.error(`Une erreur s'est produite lors de la lecture du fichier :`, err);
           return;
         }
-      
+        console.log(data)
         let tabMot = data.split('\r');
         let indRand = parseInt(Math.random() * tabMot.length);
 
